343cefc74ffb1ce801267dfb278c4a430a3f1b45
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-07-05 18:05:10 -0700

    Added the sdk for android to connect to the backend.

--- app/src/main/java/com/example/bubbel/MainActivity.kt
+++ app/src/main/java/com/example/bubbel/MainActivity.kt
@@ -4,27 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.node.modifierElementOf
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bubbel.ui.theme.BubbelTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.unit.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BubbelTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Text("File")
         }
     }
 }
@@ -41,6 +38,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BubbelTheme {
-        Greeting("Android")
+        Greeting("File")
+    }
+}
+
+@Composable
+fun LoginScreen(){
+    var xOffset = 0.1.dp
+    var yOffset  = 0.1.dp
+
+    Box(
+        modifier = Modifier.offset(
+            x = xOffset,
+            y = yOffset
+        )
+    ){
+        Text(
+            "Login", fontSize = 25.sp)
     }
 }
