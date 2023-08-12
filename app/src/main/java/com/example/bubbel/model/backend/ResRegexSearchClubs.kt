b1daa8295cb07ee41e51be29c9a62e354966a021
dav <usr21p1@gmail.com>
2023-08-11 20:00:08 -0700

    Update backend files. (auto)

---
+++ app/src/main/java/com/example/bubbel/model/backend/ResRegexSearchClubs.kt
@@ -0,0 +1,40 @@
+// To parse the JSON, install kotlin's serialization plugin and do:
+//
+// val json                = Json { allowStructuredMapKeys = true }
+// val resRegexSearchClubs = json.parse(ResRegexSearchClubs.serializer(), jsonString)
+
+package com.example.bubbel.model.backend
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import kotlinx.serialization.descriptors.*
+import kotlinx.serialization.encoding.*
+
+@Serializable
+data class ResRegexSearchClubs (
+    val error: RegexSearchClubsError? = null,
+    val res: RegexSearchClubsOut? = null
+)
+
+@Serializable
+data class RegexSearchClubsError (
+    val type: ResRegexSearchClubsErrorType,
+    val ierror: String? = null
+)
+
+@Serializable
+enum class ResRegexSearchClubsErrorType(val value: String) {
+    @SerialName("Internal") Internal("Internal"),
+    @SerialName("RegexLimit") RegexLimit("RegexLimit");
+}
+
+@Serializable
+data class RegexSearchClubsOut (
+    val clubs: List<List<Club>>
+)
+
+@Serializable
+sealed class Club {
+    class IntegerValue(val value: Long)  : Club()
+    class StringValue(val value: String) : Club()
+}
