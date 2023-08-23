2a4f8904441c63712804750c28f93fbbdf210db8
dav <usr21p1@gmail.com>
2023-08-22 20:50:02 -0700

    Update backend files. (auto)

--- app/src/main/java/com/example/bubbel/model/backend/ResGetClubProfileWithName.kt
+++ app/src/main/java/com/example/bubbel/model/backend/ResGetClubProfileWithName.kt
@@ -40,6 +40,9 @@ data class GetClubProfileWithNameOut (
     @SerializedName("display_name")
     val displayName: String? = null,
 
+    @SerializedName("is_member")
+    val isMember: Boolean? = null,
+
     val name: String,
     val owner: Long,
     val pfp: String? = null
